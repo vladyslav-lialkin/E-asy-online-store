62bcc00493c480ef33e51858bc84c8db1bd19d1f
vladyslav-ios <vlad.lyalkin@icloud.com>
2024-08-05 19:02:19 +0300

    Initial Commit

---
+++ EasyBuy_App/EasyBuy_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyBuy_AppApp.swift
+//  EasyBuy_App
+//
+//  Created by Влад Лялькін on 05.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct EasyBuy_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
